6b0ed5dfccc36a3d14eb11005fbad477e259cedd
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-05-27 12:47:19 -0400

    Clean up publishing process, display the published issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {Headers, Http, RequestOptions} from '@angular/http';
-import {AngularFireDatabase} from 'angularfire2/database';
 
 
 @Component({
@@ -9,39 +7,7 @@ import {AngularFireDatabase} from 'angularfire2/database';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  groupsDropdown;
-  date;
-  eventTitle;
-  repo = 'angularNYC/meetup';
-
-  constructor(private http: Http) {
-  }
 
 
-/*
 
-  publish() {
-    this.groups.subscribe(groups => {
-      groups.forEach(group => {
-        if (this.groupsDropdown.includes(group.label)) {
-          Object.keys(group.tasks).forEach(key => {
-            const task = group.tasks[key];
-            this.createIssue(task.name, task.description, group.label, localStorage['github-access-token']).subscribe();
-          });
-        }
-      });
-    });
-  }
-
-  createIssue(name, text, label, accessToken) {
-    const headers = new Headers({'Authorization': 'token ' + accessToken});
-    const options = new RequestOptions({headers: headers});
-    const issueData = {
-      title: `[${this.date}] [${this.eventTitle}] ${name}`,
-      body: text,
-      labels: [label]
-    };
-    return this.http.post(`https://api.github.com/repos/${this.repo}/issues`, issueData, options);
-  }
-  */
 }
--- src/app/publish/publish.component.ts
+++ src/app/publish/publish.component.ts
@@ -1,13 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {Headers, Http, RequestOptions} from '@angular/http';
+import {combineLatest} from 'rxjs/observable/combineLatest';
+import 'rxjs/add/operator/map';
 
+interface GithubIssue {
+  body: string;
+  title: string;
+  labels: Array<string>;
+}
 @Component({
   selector: 'app-publish',
   templateUrl: './publish.component.html',
   styleUrls: ['./publish.component.css']
 })
 export class PublishComponent implements OnInit {
+  repo = 'angularNYC/meetup';
+  groups = this.firebaseDb.list('/groups');
+  groupsDropdown: Array<String> = [];
+  date: string;
+  eventTitle: string;
+  issuesCreatedDate = '';
+  issuesCreated: Array<GithubIssue> = [];
 
-  constructor() { }
+  constructor(private firebaseDb: AngularFireDatabase, private http: Http) {
+  }
+
+
+  labelUrl(label) {
+    return `https://github.com/${this.repo}/issues?q=label:${label}`;
+  }
+
+  publish() {
+    this.issuesCreatedDate = this.date;
+
+    this.groups.subscribe(groups => {
+      const groupRequests = groups.filter(group => this.groupsDropdown.includes(group.label))
+        .map(group => {
+          return combineLatest(Object.keys(group.tasks).map(key => {
+            const task = group.tasks[key];
+            return this.createIssue(task.name, task.description, group.label, localStorage['github-access-token']);
+          }));
+        });
+
+
+      combineLatest(groupRequests).subscribe((result) => {
+        this.issuesCreated = [].concat.apply([], result);
+
+      });
+    });
+  }
+
+  createIssue(name, body, label, accessToken) {
+    const headers = new Headers({'Authorization': 'token ' + accessToken});
+
+    const options = new RequestOptions({headers});
+    const eventTitle = this.eventTitle ? ' ' + this.eventTitle + ':' : '';
+    const title = `[${this.date}] ${eventTitle} ${name}`;
+    const labels = [label, this.date];
+
+    return this.http.post(`https://api.github.com/repos/${this.repo}/issues`, {
+      title,
+      body,
+      labels
+    }, options).map(res => (res.json()));
+  }
 
   ngOnInit() {
   }
